4ce684541d5bd09ec0744ea88769af3384694e2a
Evandro Casa <asemlk54@hotmail.com>
2024-04-03 05:32:41 -0300

    Mudanças gerais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,102 @@
 import Image from "next/image";
+import Evandro from "../assets/image/evandro.jpeg";
+import UndrawProgramming from "../assets/image/undraw_programming.png";
+import Projeto from "../assets/image/projeto_arduino.jpg";
+import "./index.scss";
 
 export default function Home() {
   return (
-    <main className="">
-      <section className="h-screen bg-black">
-        <div className="content max-w-screen-2xl mx-auto h-screen rounded-3xl px-4"></div>
-      </section>
-    </main>
+    <div className="index-page">
+      <div className="container01">
+        <div className="sub-container">
+          <div className="dev-name">
+            <h3>Fullstack Developer</h3>
+            <h1>Evandro</h1>
+          </div>
+          <div className="container02-paragraph">
+            <p>
+              Estou constantemente buscando aprender e aplicar novos
+              conhecimentos para desenvolver soluções que não sejam apenas
+              funcionais, mas também visualmente atraentes.
+            </p>
+            <button className="button-linkedin">Linkedin</button>
+          </div>
+        </div>
+        <div className="container02-image">
+          <Image className="dev-image" src={UndrawProgramming}></Image>
+        </div>
+      </div>
+      <div className="sobre-mim">
+        <h2>Sobre mim</h2>
+
+        {/* <Image src={Evandro} className="evandro-dev"></Image> */}
+        <div className="sobre-mim_paragrafo">
+          <p>
+            Sou Evandro Fonseca Junior, um entusiasta da tecnologia e atualmente
+            estou no 3º período do curso de <b>Engenharia da Computação</b>. Com
+            21 anos de idade e {""}
+            <b>
+              dois anos de experiência sólida no desenvolvimento de software,
+            </b>{" "}
+            estou comprometido com um contínuo aprimoramento.
+            <br />
+            Minha paixão pela tecnologia remonta à infância e hoje meu interesse
+            está voltado para áreas como engenharia reversa e desenvolvimento
+            mobile nas quais busco especialização. Cresci em um ambiente
+            familiar conservador, onde os valores cristãos foram fundamentais na
+            minha formação. Essa ética acompanha minha jornada profissional,
+            guiando-me em um caminho de integridade e responsabilidade. Desde
+            cedo, dediquei-me a aprimorar meu conhecimento técnico no mundo da
+            tecnologia. Tenho habilidades suficientes de comunicação e
+            capacidade de expressar ideias com confiança, o que me torna apto a
+            falar em público com destreza. <br />
+            Além do meu comprometimento profissional, reconheço a importância do
+            equilíbrio entre vida pessoal e profissional. Por isso, pratico
+            esportes regularmente e cuido da minha saúde, acreditando que o
+            bem-estar é fundamental para o sucesso em todas as áreas da vida.
+            <br />
+            Estou sempre em busca de novos desafios que me permitam crescer e
+            contribuir de forma significativa para o campo da tecnologia.{" "}
+            <b>
+              Estou entusiasmado para explorar oportunidades e fazer parte de
+              projetos inovadores neste universo dinâmico e em constante
+              evolução.
+            </b>
+          </p>
+        </div>
+      </div>
+      <div className="container-projetos">
+        <div className="projetos-titulo">
+          <h2>Projetos</h2>
+        </div>
+        <div className="card-container">
+          <a href="/" className="hero-image-container">
+            <Image
+              className="hero-image"
+              src={Projeto}
+              alt="Spinning glass cube"
+            ></Image>
+          </a>
+          <main className="main-content">
+            <h1>
+              <b>Arduino Leitor de Temperatura e Humidade</b>
+            </h1>
+            <p>
+              Projeto de um leitor de temperatura e humidade com Arduino e
+              sensor DHT11.
+            </p>
+          </main>
+          <div className="card-attribute">
+            <Image src={Evandro} alt="avatar" className="small-avatar"></Image>
+            <p>
+              Creation of{" Evandro "}
+              <span>
+                <a href="#">Jules Wyvern</a>
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,19 @@
 import Link from "next/link";
-// import "./styles/header.css";
+import "./styles/header.scss";
 
 export function Header() {
   return (
-    <header className="w-full py-4 bg-black">
-      <nav className="flex justify-between items-center max-w-screen-xl mx-auto px-4">
-        <h2 className="text-white text-4xl w-6 hover:text-blue-500 transition-colors duration-500 ease-in-out font-bold">
-          <Link href="/">evandro.dev.br</Link>
-        </h2>
-        <ul className="flex items-center gap-3 m-2 text-2xl text-white">
-          <li className="p-4 rounded-2xl hover:text-black hover:bg-white transition-all duration-300 ease-in-out">
+    <header className="header">
+      <nav className="navigation">
+        <ul className="navigation-list">
+          <li className="navigation-item">
             <Link href="/">Inicio</Link>
           </li>
-          <li className="p-4 rounded-2xl hover:text-black hover:bg-white transition-all duration-300 ease-in-out">
+          {/* <li className="navigation-item">
             <Link href="/sobre">Sobre</Link>
-          </li>
-          <li className="p-4 rounded-2xl hover:text-black hover:bg-white transition-all duration-300 ease-in-out">
-            <Link href="/contato">Contato</Link>
+          </li> */}
+          <li className="navigation-item">
+            <Link href="#contato">Contato</Link>
           </li>
         </ul>
       </nav>
